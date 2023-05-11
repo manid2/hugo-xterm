10cf340754a74afd2247aad9b7dea1d2d60732db
Mani Kumar <manid2.work@gmail.com>
2023-05-11 15:25:38 +0530

    feat: Implement copy js script
    * Show copy buttons only when JavaScript is enabled.
    
    Refactor:
    
    * Sort the scripts list in base layout.
    * Remove small fonts for highlight toolbar.
    * Add hide class to show/hide elements and for scripting.
    * Add 'js-btn-copy-code' to copy button class list to identify as the
      copy button.
    * Add empty print js for later implementation.
    
    Original-Commit:
    
    https://gitlab.com/manid2/manid2.gitlab.io/-/commit/2a2943a42d034f1aa484b92289f8ec61fd2987ba

---
+++ assets/scripts/copy.js
@@ -0,0 +1,39 @@
+const hiTextBlock = document.querySelectorAll(".highlight-wrapper")
+
+hiTextBlock.forEach(function (hiTextBlock) {
+  const hiToolbar = hiTextBlock.querySelector(".highlight-toolbar");
+  const hiText = hiTextBlock.querySelector(".highlight");
+  const copyButton = hiToolbar.querySelector(".js-btn-copy-code");
+  copyButton.classList.remove("hide");
+
+  /* Borrowed from adityatelange/hugo-PaperMod theme. */
+  function copyingDone() {
+    copyButton.innerHTML = "Copied!";
+    setTimeout(() => {
+      copyButton.innerHTML = "Copy";
+    }, 2000);
+  }
+
+  /* copy code in pre > code blocks */
+  copyButton.addEventListener("click", () => {
+
+    // Use clipboard API if available.
+    if ('clipboard' in navigator) {
+      navigator.clipboard.writeText(hiText.innerText);
+      copyingDone();
+      return;
+    }
+
+    // Fallback to selection and copy
+    const range = document.createRange();
+    range.selectNodeContents(hiText);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    try {
+      document.execCommand('copy');
+      copyingDone();
+    } catch (e) { };
+    selection.removeRange(range);
+  });
+});
